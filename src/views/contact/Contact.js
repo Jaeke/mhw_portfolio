170fc98fc494bbaadfede67c842a943d1a2d357e
jaeke <jakechen825@gmail.com>
2022-04-03 13:04:47 -0400

    FINISHED: 	- finished about and contact page with styling and content 		- still missing better head shot for about page
    IN PROGRESS:
    	- giving links and functions to icon button
    	- contact page might need more content
    	- email form component

--- src/views/contact/Contact.js
+++ src/views/contact/Contact.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import { MenuToggler } from '../../components';
+import './Contact.style.scss';
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact_page_container">
       <MenuToggler contact={true} />
-      <p>This is contact page</p>
+      <motion.div
+        className="contact_content"
+        initial={{ x: '-200px', opacity: 0 }}
+        animate={{
+          x: '0px',
+          opacity: 1,
+        }}
+        transition={{
+          type: 'tween',
+          duration: 1,
+        }}
+      >
+        <div className="contact_body">
+          <span className="contact_header">contact</span>
+          <div>Mark@MHWstudio.com</div>
+          <div>(917) 232-9955 </div>
+          <div>New York, NY</div>
+        </div>
+      </motion.div>
     </div>
   );
 };
