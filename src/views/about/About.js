8c70f402ad2b91d0d534d633e7b2ed56cc6522df
jaeke <jakechen825@gmail.com>
2022-04-03 12:23:48 -0400

    set up base about page

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,11 +1,89 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import { MenuToggler } from '../../components';
+import './About.style.scss';
 
 const About = () => {
   return (
-    <div>
+    <div className="about_page_container">
       <MenuToggler about={true} />
-      <h1>This is the About page</h1>
+      <motion.div
+        className="bio_picture"
+        initial={{ x: '200px', opacity: 0 }}
+        animate={{
+          x: '0px',
+          opacity: 1,
+        }}
+        transition={{
+          type: 'tween',
+          duration: 1.2,
+        }}
+      />
+      <div className="bio_container">
+        <motion.div
+          className="bio_header"
+          initial={{ x: '-200px', opacity: 0 }}
+          animate={{
+            x: '0px',
+            opacity: 1,
+          }}
+          transition={{
+            type: 'tween',
+            duration: 1.2,
+          }}
+        >
+          Mark H. Wesley
+        </motion.div>
+        <motion.div
+          className="bio_content"
+          initial={{ y: '200px', opacity: 0 }}
+          animate={{
+            y: '0px',
+            opacity: 1,
+          }}
+          transition={{
+            type: 'tween',
+            duration: 1.2,
+          }}
+        >
+          <p>
+            Designer Mark Wesley seamlessly blends your vision for your project
+            with interior styling that reflects the feeling you’re looking to
+            create. Understanding the power of storytelling through design, Mark
+            is highly valued for his innate ability to consistently understand
+            that all creative executions result in a new feeling, increased
+            comfort or increased sales.
+          </p>
+
+          <p>
+            Mark’s design career began with lifestyle brands and with his
+            natural talent and refined eye for detail, proportion, and color, he
+            was quickly discovered by fashion house Ralph Lauren.
+          </p>
+
+          <p>
+            Mark’s creative work for Ralph’s men’s, women’s and home lines
+            debuted in hundreds of spaces including high-fashion runways, retail
+            interiors and window designs for premier flagship stores nationwide
+            for over 20 years.
+          </p>
+
+          <p>
+            MHW Studio provides interior design, creative direction,
+            installation projects, custom furniture and lighting design for
+            hospitality, commercial and residential clients.
+          </p>
+
+          <p>
+            Mark believes his job is about making the design process easy for
+            you, with results that feel good. Through his joy for music, travel
+            and learning, he is consistently seeing and refining his eye to see
+            the world in new ways and matching that refined perspective to your
+            project.
+          </p>
+        </motion.div>
+      </div>
     </div>
   );
 };
