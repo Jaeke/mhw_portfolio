fbfef6c8641d5c9d91d40a16039a92bcdb7c5ae8
jaeke <jakechen825@gmail.com>
2022-05-30 22:51:31 -0400

    finished implementing basic react form with EmailJS, it works now but will try to connect it with formik instead and also toast notification as well

--- src/views/email/Email.js
+++ src/views/email/Email.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Formik } from 'formik';
 import { Form, FormikDebug, Input, SubmitButton } from 'formik-antd';
+import emailjs from '@emailjs/browser';
 
 import './Email.style.scss';
 import { MenuToggler } from '../../components';
 import { formValidation } from '../../Utils/formValidation';
 
 const Email = () => {
+  const form = useRef();
+
   const [width, setWidth] = useState(window.innerWidth);
 
   const updateWidthAndHeight = () => {
@@ -18,10 +21,44 @@ const Email = () => {
     return () => window.removeEventListener('resize', updateWidthAndHeight);
   }, []);
 
-  const handleSubmit = (values, actions) => {
-    console.log('submitted values: ', values);
-    actions.setSubmitting(false);
-    actions.resetForm();
+  // const handleSubmit = (values, actions) => {
+  //   console.log('submitted values: ', values);
+
+  //   emailjs
+  //     .sendForm(
+  //       'service_2uqclhw',
+  //       'template_hc7wad6',
+  //       form.current,
+  //       'WVmExMwMvYninowHR'
+  //     )
+  //     .then(
+  //       (result) => {
+  //         alert('SUCCESS');
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         alert('FAILED');
+  //         console.log(error.text);
+  //       }
+  //     );
+
+  //   actions.setSubmitting(false);
+  //   actions.resetForm();
+  // };
+
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(serviceId, templateId, form.current, userId).then(
+      (result) => {
+        alert('SUCCESS');
+        console.log(result.text);
+      },
+      (error) => {
+        alert('FAILED');
+        console.log(error.text);
+      }
+    );
   };
 
   return (
@@ -30,7 +67,8 @@ const Email = () => {
         {width > 716 ? <MenuToggler /> : ''}
         <div className="email_form_container">
           <span className="form_header">connect with me</span>
-          <Formik
+          {/* <Formik
+            ref={form}
             initialValues={{
               name: '',
               email: '',
@@ -75,9 +113,59 @@ const Email = () => {
               <div className="form_btn">
                 <SubmitButton>Send</SubmitButton>
               </div>
-              {/* <FormikDebug /> */}
             </Form>
-          </Formik>
+          </Formik> */}
+          <form ref={form} onSubmit={sendForm}>
+            {/* <!-- name --> */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="name"
+                name="name"
+                className="form-control"
+                id="name"
+                placeholder="enter your name"
+              />
+            </div>
+
+            {/* <!-- email --> */}
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                id="email"
+                placeholder="enter your email"
+              />
+            </div>
+
+            {/* <!-- subject --> */}
+            <div className="form-group">
+              <label htmlFor="subject">Subject</label>
+              <input
+                type="text"
+                name="subject"
+                className="form-control"
+                id="subject"
+                placeholder="enter email subject"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea
+                className="form-control"
+                id="message"
+                name="message"
+                rows="5"
+              ></textarea>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </>
