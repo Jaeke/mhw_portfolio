0ffd5b9b6420b13ecbac56b6d8c559fb9a595720
jaeke <jakechen825@gmail.com>
2022-07-08 15:08:37 -0400

    clean up code in email btn

--- src/components/sidebar/iconBtn/Email.js
+++ src/components/sidebar/iconBtn/Email.js
@@ -3,24 +3,10 @@ import { Envelope } from 'phosphor-react';
 import { NavLink } from 'react-router-dom';
 
 import './IconBtn.style.scss';
-import { uiActions } from '../../../store/UI/ui-slice';
-import { useDispatch } from 'react-redux';
 
 const Email = () => {
-  const dispatch = useDispatch();
-
-  const emailIconHandler = () => {
-    console.log('email');
-    // dispatch(uiActions.setSideDrawerVisible());
-  };
-
   return (
-    <NavLink
-      className="icon"
-      weight="thin"
-      to="/email"
-      onClick={emailIconHandler}
-    >
+    <NavLink className="icon" weight="thin" to="/email">
       <Envelope />
     </NavLink>
   );
