ceef3fca99b35afedcdb83ba9c1e1c6de67c474b
jaeke <jakechen825@gmail.com>
2022-07-10 04:06:52 -0400

    change email file name

--- src/views/index.js
+++ src/views/index.js
@@ -2,6 +2,6 @@ import Home from './home/HomePage';
 import About from './about/AboutPage';
 import Projects from './gallery/ProjectsPage';
 import Contact from './contact/ContactPage';
-import Email from './email/Email';
+import Email from './email/EmailPage';
 
 export { Home, About, Projects, Contact, Email };
