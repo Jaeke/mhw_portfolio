b6a4531c04e66ca80b114b864715842a4b568994
jaeke <jakechen825@gmail.com>
2022-07-10 05:35:57 -0400

    change import and remove views index

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import {
-  AboutPage,
-  HomePage,
-  ProjectsPage,
-  ContactPage,
-  EmailPage,
-} from './Views';
+import HomePage from './Views/home/HomePage';
+import AboutPage from './Views/about/AboutPage';
+import ProjectsPage from './Views/gallery/ProjectsPage';
+import ContactPage from './Views/contact/ContactPage';
+import EmailPage from './Views/email/EmailPage';
 
 const appRoutes = () => {
   return (
     <Routes>
-      {/* <Route exact path="/" element={<HomePage />} />
-      <Route exact path="/home" element={<HomePage />} /> */}
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/home" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/gallery" element={<ProjectsPage />} />
       <Route path="/contact" element={<ContactPage />} />
--- src/views/index.js
+++
@@ -1,7 +0,0 @@
-// import HomePage from './home/HomePage';
-import AboutPage from './about/AboutPage';
-import ProjectsPage from './gallery/ProjectsPage';
-import ContactPage from './contact/ContactPage';
-import EmailPage from './email/EmailPage';
-
-export { AboutPage, ProjectsPage, ContactPage, EmailPage };
